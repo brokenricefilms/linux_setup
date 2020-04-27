750b3d8b7313c923ed051c5c5e0cf32379017826
nicholas <nicholaspham2311@gmail.com>
2020-04-27 20:09:51 +0700

    Ok

--- step1_installNecessary.sh
+++ step1_installNecessary.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # necessary
 sudo apt-get install tmux curl wget git fish neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server flameshot python3-pip tldr net-tools xclip speedtest-cli neovim fd-find aria2 tree gimp apt-transport-https cowsay npm vifm fzf npm -y
+
+sudo npm install --global prettier
 
 # teni
 sudo add-apt-repository ppa:teni-ime/ibus-teni
@@ -20,15 +22,8 @@ sudo apt install brave-browser -y
 
 # nvim setup
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # moving fish from bash
 echo "Enter your passwork"
 chsh -s "$(which fish)"
-
-# make gnome terminal fancy
-wget https://raw.githubusercontent.com/NLKNguyen/papercolor-16/master/gnome-terminal/base16-papercolor-light.sh
-bash base16-papercolor-light.sh
-rm base16-papercolor-light.sh
-
-sudo npm install --global prettier
--- step2_gitSetup.sh
+++ step2_gitSetup.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# chance this dude
 # echo "
 # [user]
 	# name = nicholas
 	# email = nicholaspham2311@gmail.com
 # " > ~/.gitconfig
 
-mkdir git ; cd ~/git/
+mkdir ~/git ; cd ~/git/
 
 git clone git@github.com:nicholaspham2311/dotfiles.git
 # git clone git@github.com:nicholaspham2311/lazyscript.git
@@ -15,15 +16,17 @@ git clone git@github.com:nicholaspham2311/dotfiles.git
 # git clone git@github.com:nicholaspham2311/FreecodeCampProject.git
 # git clone git@github.com:nicholaspham2311/vimium_dark_theme.git
 
-mkdir ~/.config/nvim/ 
+mkdir ~/.config/nvim/
+mkdir ~/.config/fish/
+mkdir ~/.config/fish/functions/
+mkdir ~/.config/vifm/
+
 cp ~/git/dotfiles/git/vifm/* ~/.config/vifm/
+cp ~/git/dotfiles/git/vifm/colors/* ~/.config/vifm/colors/
 cp ~/git/dotfiles/nvim/init.vim ~/.config/nvim/
-cp ~/git/dotfiles/vim/.vimrc ~/
 cp ~/git/dotfiles/ssh/config ~/.ssh/
 cp ~/git/dotfiles/tmux/.tmux.conf ~/
 cp ~/git/dotfiles/fish/config.fish ~/.config/fish/
-mkdir ~/.config/fish/functions/
 cp ~/git/dotfiles/fish/fish_prompt.fish ~/.config/fish/functions/
 cp ~/git/dotfiles/git/.gitconfig ~/ 
 cp ~/git/dotfiles/.selected_editor ~/
-cp ~/git/ok/ssh/* ~/.ssh/
