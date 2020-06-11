35ae2fbdea0b1693dd4af2e48939b230ec88502c
thuan <phamtanthuan2311@gmail.com>
2020-06-11 20:12:52 +0700

    Feed. You. Stuff. No time.

--- step2_gitSetup.sh
+++ step2_gitSetup.sh
@@ -9,12 +9,14 @@
 
 mkdir ~/git ; cd ~/git/
 
-git clone https://github.com/nicholaspham2311/dotfiles.git
-# git clone git@github.com:nicholaspham2311/lazyscript.git
-# git clone git@github.com:nicholaspham2311/linux_setup.git
-# git clone git@github.com:nicholaspham2311/dataLab.git
-# git clone git@github.com:nicholaspham2311/FreeCodeCampProject.git
-# git clone git@github.com:nicholaspham2311/vimium_dark_theme.git
+git clone https://github.com/thuanpham2311/dotfiles.git
+# git clone git@github.com:thuanpham2311/dotfiles.git
+# git clone git@github.com:thuanpham2311/lazyscript.git
+# git clone git@github.com:thuanpham2311/linux_setup.git
+# git clone git@github.com:thuanpham2311/dataLab.git
+# git clone git@github.com:thuanpham2311/FreeCodeCampProject.git
+# git clone git@github.com:thuanpham2311/vimium_dark_theme.git
+# git clone git@github.com:thuanpham2311/windowsSetup.git
 
 mkdir ~/.config/nvim/
 mkdir ~/.config/fish/
