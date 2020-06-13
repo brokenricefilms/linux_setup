db5cf1aafa5c1640d64e7a8005100923d79d612b
thuan <phamtanthuan2311@gmail.com>
2020-06-13 16:52:08 +0700

    Argh! About to give up :(

--- step1_installNecessary.sh
+++ step1_installNecessary.sh
@@ -6,6 +6,7 @@ sudo npm install --global prettier
 # go to tinypng get tinypng dev API and paste it to file .tinypng at $HOME
 sudo npm install --global tinypng-cli
 sudo npm install --global browser-sync
+sudo npm install --global yarn
 
 # install font
 sudo apt install -y fonts-noto-mono
@@ -16,15 +17,6 @@ sudo apt update
 sudo apt install ibus-teni
 ibus restart
 sudo usermod -a -G input $USER
-
-## git ...
-sudo snap innstall grv -y
-
-# brave
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
 
 # nvim setup
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
--- step2_gitSetup.sh
+++ step2_gitSetup.sh
@@ -1,35 +1,25 @@
 #!/usr/bin/env bash
 
-# chance this dude
-# echo "
-# [user]
-	# name = thuan
-	# email = phamtanthuan2311@gmail.com
-# " > ~/.gitconfig
-
 mkdir ~/git ; cd ~/git/
 
-git clone https://github.com/thuanpham2311/dotfiles.git
-# git clone git@github.com:thuanpham2311/dotfiles.git
-# git clone git@github.com:thuanpham2311/lazyscript.git
-# git clone git@github.com:thuanpham2311/linux_setup.git
-# git clone git@github.com:thuanpham2311/dataLab.git
-# git clone git@github.com:thuanpham2311/FreeCodeCampProject.git
-# git clone git@github.com:thuanpham2311/vimium_dark_theme.git
-# git clone git@github.com:thuanpham2311/windowsSetup.git
-# git clone git@github.com:thuanpham2311/termuxSetup.git
+git clone git@github.com:thuanpham2311/dotfiles.git
+git clone git@github.com:thuanpham2311/lazyscript.git
+git clone git@github.com:thuanpham2311/linux_setup.git
+git clone git@github.com:thuanpham2311/dataLab.git
+git clone git@github.com:thuanpham2311/FreeCodeCampProject.git
+git clone git@github.com:thuanpham2311/vimium_dark_theme.git
+git clone git@github.com:thuanpham2311/windowsSetup.git
+git clone git@github.com:thuanpham2311/termuxSetup.git
 
 mkdir ~/.config/nvim/
 mkdir ~/.config/fish/
 mkdir ~/.config/fish/functions/
 mkdir ~/.config/vifm/
 
-cp ~/git/dotfiles/git/vifm/* ~/.config/vifm/
-cp ~/git/dotfiles/git/vifm/colors/* ~/.config/vifm/colors/
-cp ~/git/dotfiles/nvim/init.vim ~/.config/nvim/
-cp ~/git/dotfiles/ssh/config ~/.ssh/
+cp -r ~/git/dotfiles/vifm/* ~/.config/vifm/
+cp -r ~/git/dotfiles/nvim/* ~/.config/nvim/
+cp -r ~/git/dotfiles/ssh/* ~/.ssh/
 cp ~/git/dotfiles/tmux/.tmux.conf ~/
-cp ~/git/dotfiles/fish/config.fish ~/.config/fish/
-cp ~/git/dotfiles/fish/fish_prompt.fish ~/.config/fish/functions/
+cp -r ~/git/dotfiles/fish/* ~/.config/fish/
 cp ~/git/dotfiles/git/.gitconfig ~/ 
 cp ~/git/dotfiles/.selected_editor ~/
