e50c04c33b9428a0a3ad887e54a1be0389db7a2d
thuan <phamtanthuan2311@gmail.com>
2020-08-18 22:26:30 +0700

    It works on my computer

---
+++ archStep1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# necessary
+sudo pacman -S adb tmux curl wget git fish neofetch htop mpv gnome-tweaks trash-cli flameshot tldr net-tools xclip speedtest-cli neovim fd aria2 tree gimp cowsay npm vifm fzf npm imagemagick ruby ripgrep brave-beta
+
+sudo npm install --global prettier
+# go to tinypng get tinypng dev API and paste it to file .tinypng at $HOME
+sudo npm install --global tinypng-cli
+sudo npm install --global browser-sync
+sudo npm install --global yarn
+sudo npm install -g neovim
+
+sudo pip3 install neovim
+
+# ruby
+# optional neovim
+gem install neovim
+
+# nvim setup
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
