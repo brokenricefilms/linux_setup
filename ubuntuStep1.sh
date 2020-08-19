963c6d24c43f9fc01a9e0412baee5aa8159785a9
thuan <phamtanthuan2311@gmail.com>
2020-08-19 15:01:19 +0700

    eppic fail Hanlle

--- ubuntuStep1.sh
+++ ubuntuStep1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # necessary
-sudo apt-get install adb tmux curl wget git fish neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server flameshot python3-pip tldr net-tools xclip speedtest-cli neovim fd-find aria2 tree gimp apt-transport-https cowsay npm vifm fzf npm clangd jupyer-notebook fonts-noto-mono gnome-shell-pomodoro simplescreenrecorder imagemagick ruby-dev ripgrep -y
+sudo apt-get install adb tmux curl wget git fish neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server flameshot python3-pip tldr net-tools xclip speedtest-cli neovim fd-find aria2 tree gimp apt-transport-https cowsay npm vifm fzf npm clangd fonts-noto-mono gnome-shell-pomodoro simplescreenrecorder imagemagick ruby-dev ripgrep -y
 
 sudo npm install --global prettier
 # go to tinypng get tinypng dev API and paste it to file .tinypng at $HOME
@@ -13,19 +13,22 @@ sudo npm install -g neovim
 # optional neovim
 sudo gem instal neovim
 
-# teni
-sudo add-apt-repository ppa:teni-ime/ibus-teni
-sudo apt update
-sudo apt install ibus-teni -y
+sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+sudo apt-get update
+sudo apt-get install ibus-bamboo
 ibus restart
-sudo usermod -a -G input $USER
 
 # brave
 sudo apt install apt-transport-https curl
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+curl -s https://brave-browser-apt-beta.s3.brave.com/brave-core-nightly.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-prerelease.gpg add -
+
+echo "deb [arch=amd64] https://brave-browser-apt-beta.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-beta.list
+
 sudo apt update
-sudo apt install brave-browser
+
+sudo apt install brave-browser-beta
+
 
 # nvim setup
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
