2fa718f94c7242d77f09854b70b5c667ce807e97
thuan <phamtanthuan2311@gmail.com>
2020-12-03 11:00:50 +0700

    need another beer

--- ubuntuStep1.sh
+++ ubuntuStep1.sh
@@ -11,7 +11,7 @@ sudo apt update -y
 sudo apt install brave-browser -y
 
 # necessary
-sudo apt install jupyter zsh adb tmux curl wget git neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server flameshot ipython3 python3-pip tldr net-tools xclip speedtest-cli neovim fd-find aria2 tree gimp  cowsay fzf npm clangd fonts-noto-mono gnome-shell-pomodoro simplescreenrecorder imagemagick  ripgrep foliate zsh zsh-autosuggestions zsh-syntax-highlighting ibus-bamboo unrar ctags moreutils nnn kitty -y
+sudo apt install jupyter zsh adb tmux curl wget git neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server flameshot ipython3 python3-pip tldr net-tools xclip speedtest-cli neovim fd-find aria2 tree gimp  cowsay fzf npm clangd fonts-noto-mono gnome-shell-pomodoro simplescreenrecorder imagemagick  ripgrep foliate zsh zsh-autosuggestions zsh-syntax-highlighting ibus-bamboo unrar ctags moreutils nnn kitty autokey-gtk -y
 
 sudo pip3 install jupyterlab
 
