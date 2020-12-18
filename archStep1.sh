1792836b6119cfde0e85e5e75eadb4640bf34281
thuan <phamtanthuan2311@gmail.com>
2020-12-18 23:01:11 +0700

    I will not apologize for art.

--- archStep1.sh
+++ archStep1.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu --noconfirm
 
 sudo pamac build ibus-bamboo --noconfirm
-sudo pacman -S yay tmux neofetch htop mpv trash-cli flameshot tldr net-tools xclip speedtest-cli neovim fd tree gimp cowsay npm vifm npm ruby ripgrep brave kitty nnn exa noto-fonts-emoji lua aria2 onlyoffice-desktopeditors foliate tig clang --noconfirm
+sudo pacman -S yay tmux neofetch htop mpv trash-cli flameshot tldr net-tools xclip speedtest-cli neovim fd tree gimp cowsay vifm ruby ripgrep brave kitty nnn exa noto-fonts-emoji lua aria2 onlyoffice-desktopeditors foliate tig clang nodejs npm --noconfirm
 
 yay -S autokey gnome-shell-pomodoro universal-ctags --noconfirm
 
