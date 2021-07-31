979c3a98555d9238ed8b7c6bed7def0be80c5b0c
thuan <phamtanthuan2311@gmail.com>
2021-07-31 12:02:44 +0700

    style(ubuntu step 1): clean code

--- ubuntuStep1.sh
+++ ubuntuStep1.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
-sudo apt install apt-transport-https -y
+sudo apt install apt-transport-https curl gnupg -y
+sudo apt update -y
+
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo add-apt-repository ppa:apandada1/foliate
 
-# brave
-sudo apt install apt-transport-https curl gnupg
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
 sudo apt install brave-browser -y
 
-# necessary
 sudo apt install copyq zsh tmux curl wget git neofetch htop mpv gnome-tweaks trash-cli openssh-client openssh-server flameshot ipython3 python3-pip tldr net-tools xclip speedtest-cli neovim fd-find aria2 tree cowsay fzf npm clangd fonts-noto-mono gnome-shell-pomodoro simplescreenrecorder ripgrep foliate zsh ibus-bamboo unrar moreutils nnn kitty exa universal-ctags lua5.4 node-typescript bashtop tig bat libx11-dev libxft-dev libharfbuzz-dev -y
-
 
 # use npm install --global without sudo
 npm config set prefix ~/.npm
@@ -30,11 +28,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 sudo python3 -m pip install --user --upgrade pynvim
 sudo python3 -m pip install --user --upgrade pylint
 
-# ruby
 sudo apt-get install ruby-dev ruby-full build-essential zlib1g-dev -y
 gem install jekyll bundler neovim
 
-# nvim setup
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -47,7 +43,6 @@ echo "Switch to Zsh from Bash"
 echo "Enter your passwork"
 chsh -s "$(which zsh)"
 
-# ubuntu
 mkdir ~/app ; cd ~/app
 git clone https://github.com/pop-os/shell
 cd ~/app/shell
