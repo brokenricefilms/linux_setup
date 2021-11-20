4d7057839552254ce413dd7acfb651c58c836f1f
Thuan Pham <phamtanthuan2311@gmail.com>
2021-11-20 14:12:40 +0700

    🐞fix(step2): ln the gitconfig, make gitconfig search able with fzf

--- step2.sh
+++ step2.sh
@@ -32,7 +32,7 @@ ln -sf ~/dotfiles/nvim ~/.config
 ln -sf ~/dotfiles/kitty/ ~/.config/
 ln -sf ~/dotfiles/bat/ ~/.config/
 ln -sf ~/dotfiles/copyq.conf ~/.config/copyq/copyq.conf
-ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.selected_editor ~/.selected_editor
 ln -sf ~/sync/ok/.fonts ~/.fonts ; cd ~/dotfiles/.fonts ; fc-cache -v
 ln -sf ~/dotfiles/gtk.css ~/.config/gtk-3.0/gtk.css
