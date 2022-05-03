25f7a841fdd536b9336c3fb5057833771cda52ae
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-03 08:06:46 +0700

    switch to firefox default

--- default.sh
+++ default.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 
-xdg-mime default file_chrome.desktop image/jpeg
-xdg-mime default file_chrome.desktop image/pdf
-xdg-mime default file_chrome.desktop image/gif
-xdg-mime default file_chrome.desktop image/svg+xml
+xdg-mime default file_browser.desktop image/jpeg
+xdg-mime default file_browser.desktop image/pdf
+xdg-mime default file_browser.desktop image/gif
+xdg-mime default file_browser.desktop image/svg+xml
