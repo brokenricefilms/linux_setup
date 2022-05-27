5040cc554fc9488a84354b3badc68da38e316830
Thuan Pham <phamtanthuan2311@gmail.com>
2022-05-27 23:57:07 +0700

    🎨refactor(step1.sh): no brave,  fedora install rust, go, deno

--- step1.sh
+++ step1.sh
@@ -15,16 +15,14 @@ fi
 # for fedora
 if hash dnf 2>/dev/null; then
   sudo dnf install dnf-plugins-core
-  sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-  sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-  sudo dnf install brave-browser
 
   sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
   sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:lamlng/Fedora_33/home:lamlng.repo
   sudo dnf install ibus-bamboo
 
-  sudo dnf install copyq tmux curl wget git neofetch htop gnome-tweaks trash-cli flameshot python3-pip tldr net-tools xclip speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay npm fzf npm ffmpeg youtube-dl mpv gnome-shell-extension-pop-shell tig kitty ripgrep unrar exa moreutils foliate bat util-linux-user zsh cronie git-delta wl-copy java-devel git-clang-format -y
+  sudo dnf install tmux curl wget git neofetch htop gnome-tweaks trash-cli flameshot python3-pip tldr net-tools xclip speedtest-cli neovim python3-neovim fd-find aria2 tree cowsay npm fzf npm ffmpeg youtube-dl mpv tig kitty ripgrep unrar exa moreutils foliate bat util-linux-user zsh cronie git-delta wl-clipboard java-devel git-clang-format rust cargo go -y
+  curl -fsSL https://deno.land/install.sh | sh
 fi
 
 # for arch base
