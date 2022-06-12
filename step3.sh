0744b5f440160dc08e670d4ea3f7e33174a46eb3
Thuan Pham <phamtanthuan2311@gmail.com>
2022-06-12 11:21:47 +0700

    🌟feature(step3.sh): install zsh tmux auto title

--- step3.sh
+++ step3.sh
@@ -9,9 +9,9 @@ cd -
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cd ~/dotfiles/zsh/functions/
-git clone https://github.com/zsh-users/zsh-syntax-highlighting
-git clone https://github.com/Aloxaf/fzf-tab
-git clone https://github.com/chitoku-k/fzf-zsh-completions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/Aloxaf/fzf-tab
+git clone --depth=1 https://github.com/chitoku-k/fzf-zsh-completions
 cd
 
 curl -fsSL https://starship.rs/install.sh | bash
